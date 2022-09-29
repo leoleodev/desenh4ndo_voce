278e1641b7b46a3e5c325c6bc6f7e3d95e127812
Leonardo <leonardoj.m.4@gmail.com>
2022-09-29 11:21:27 -0300

    start depositions section

---
+++ src/components/DepositionsSection.tsx
@@ -0,0 +1,27 @@
+
+import Styles from '../styles/modules/DepositionsSection.module.css'
+
+
+export function DepositionsSection(){
+    return(
+        <section className={Styles.depositionContainer}>
+
+            <h2>Depoimentos e Estatisticas</h2>
+
+            <div className={Styles.depositionContent}>
+                <img src="" alt="Icone de depoimentos" />
+                <div className={Styles.commentsArea}>
+                    {/* card de comentário */}
+                    {/* card de comentário */}
+                    {/* card de comentário */}
+                </div>
+            </div>
+            <div className={Styles.complementaryArea}>
+
+                {/* card circuilo */}
+                
+            </div>
+
+        </section>
+    );
+}
--- src/components/FromSection.tsx
+++ src/components/FromSection.tsx
@@ -20,7 +20,7 @@ export function FormSection(){
                 </h2>
                 <form>
                     <input type="text" name="name" id="name" placeholder='Nome'/>
-                    <input type="text" name="lastName" id="lastName" placeholder='Sobre nome'/>
+                    <input type="text" name="lastName" id="lastName" placeholder='Sobrenome'/>
                     <input type="email" name="email" id="email" placeholder='Email'/>
                     <input type="tel" name="tel" id="tel" placeholder="Telefone" />
                     <textarea name="textArea" id="textArea" placeholder='Deixe-me uma mensagem'></textarea>
