001b60af0705793cad14342a995ae1b61b8780f7
Leonardo <leonardoj.m.4@gmail.com>
2022-10-04 20:46:01 -0300

    card circle partially finished

--- src/components/ButtonCircle.tsx
+++ src/components/ButtonCircle.tsx
@@ -5,14 +5,18 @@ export function ButtonCircle(){
     return(
         <section className={Styles.complemetaryAreaContainer}>
 
-            <div className={Styles.circle}>
-                <div className={Styles.data}>
+            <div className={Styles.time}>
+                <div className={Styles.circle}>
+                    <div className={Styles.dot}></div>
                     <svg>
-                        <circle cx={70} cy={70} r={70}></circle>
-                        <circle cx={70} cy={70} r={70}></circle>
+                        <circle cx="70" cy="70" r="70"></circle>
+                        <circle cx="70" cy="70" r="70"></circle>
                     </svg>
+                    <div className={Styles.info}>90%</div>
                 </div>
             </div>
+
+            <h3>Rapidez na entrega</h3>
 
         </section>
     );
