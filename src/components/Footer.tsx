1e963fed75d57d664b5de7c0d2b625f3c5bc9438
Leonardo <leonardoj.m.4@gmail.com>
2022-10-07 22:15:16 -0300

    footer in progress

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,12 @@
 
 import Styles from '../styles/modules/Footer.module.css'
 
+import iconWhatsapp from '../images/icons/whatsappIcon.svg'
+import iconInstagram from '../images/icons/instagramIcon.svg'
+import iconFacebook from '../images/icons/facebookIcon.svg'
+import iconTime from '../images/icons/clock.svg'
+import iconClose from '../images/icons/close.svg'
+
 export function Footer(){
     return(
         <div className={Styles.footerContainer}>
@@ -16,18 +22,20 @@ export function Footer(){
             <div className={Styles.socialMedia}>
                 <h2>Redes Sociais</h2>
                 <ul>
-                    <li> <a href="#"> <img src="" alt="Icone do whatsapp"/> Whatsapp </a> </li>
-                    <li> <a href="#"> <img src="" alt="Icone do Instagram"/> Instagram </a> </li>
-                    <li> <a href="#"> <img src="" alt="Icone do Facebook"/> Facebook </a> </li>
+                    <li> <a href="#"> <img src={iconWhatsapp} alt="Icone do whatsapp"/> Whatsapp </a> </li>
+                    <li> <a href="#"> <img src={iconInstagram} alt="Icone do Instagram"/> Instagram </a> </li>
+                    <li> <a href="#"> <img src={iconFacebook} alt="Icone do Facebook"/> Facebook </a> </li>
                 </ul>
             </div>
 
             <div className={Styles.schedules}>
                 <h2>Horários</h2>
                 <ul>
-                    <li> <img src="#" alt="Icone de tempo" /> Segunda à Sexta das 8h - 17h </li>
-                    <li> <img src="#" alt="Icone de tempo" /> Sábados das 8h - 12h </li>
-                    <li> <img src="#" alt="Icone de negação" /> Domingo Fechado </li>
+
+                    <li> <a href="#"> <img src={iconTime} alt="Icone de tempo"/> Seg à Sex de 8h - 17h </a> </li>
+                    <li> <a href="#"> <img src={iconTime} alt="Icone de tempo"/> Seg à Sex de 8h - 17h </a> </li>
+                    <li> <a href="#"> <img src={iconTime} alt="Icone de negação"/> Fechado </a> </li>
+                    
                 </ul>
             </div>
             
